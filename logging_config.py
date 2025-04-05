30c786610d4e010289b2ae7ab001312cbdb5dcd3
John Haase <haas1681@gmail.com>
2025-04-04 20:24:33 -0400

    error handling

--- logging_config.py
+++ logging_config.py
@@ -10,6 +10,7 @@ log_date = datetime.now().strftime("%Y-%m-%d")
 log_file = f"logs/{log_date}.txt"
 logging.basicConfig(
     filename=log_file,
+    filemode="a",  # Append to the log file instead of overwriting
     format="%(asctime)s - %(name)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
