4c5cb1edfdb0157dd8c07b4fb8675ab39434cb29
John Haase <haas1681@gmail.com>
2025-07-13 12:29:45 -0400

    logging

--- logging_config.py
+++ logging_config.py
@@ -14,7 +14,7 @@ log_file = f"logs/{log_date}.txt"
 logging.basicConfig(
     filename=log_file,
     filemode="a",  # Append to the log file instead of overwriting
-    format="%(asctime)s - %(name)s: %(message)s",
+    format="%(asctime)s - %(name)s - %(lineno)d: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
 )
